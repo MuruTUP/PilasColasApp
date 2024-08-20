897692ee1f0ec36f7f67404bb0b4f39a22d3e275
alumno <alumno@GI.FRC.UTN.EDU.AR>
2024-08-20 15:13:20 -0300

    QUe onda gordo

---
+++ Clases/Pila.cs
@@ -0,0 +1,47 @@
+﻿using PilasColasApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilasColasApp.Clases
+{
+
+    internal class Pila : IColleccion
+    {
+        protected Object[] array;
+        protected int contador;
+
+        public Pila(int size)
+        {
+            array = new Object[size];
+            contador = -1;
+        }
+
+        public bool añadir(object elemento)
+        {
+            
+        }
+
+        public bool estaVacia(Collection<object> coleccion)
+        {
+            return coleccion.Any();
+        }
+
+        public object extraer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object primero(Collection<object> coleccion)
+        {
+            if (!coleccion.Any())
+            {
+                return "No existe" ;
+            }
+            return coleccion[0];
+        }
+    }
+}
---
+++ Interfaces/IColeccion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilasColasApp.Interfaces
+{
+    public interface IColleccion
+    {
+        bool estaVacia();
+        Object extraer();
+        Object primero(object elemento);
+        bool añadir(object elemento);
+
+    }
+}
